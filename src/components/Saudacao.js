9d22c5b04b5d60e96a117a312b99270af78bd4f8
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-01-08 10:04:13 -0300

    State Lift

---
+++ src/components/Saudacao.js
@@ -0,0 +1,9 @@
+const Saudacao = ({nome}) => {
+  const gerarSaudacao = (algumNome) => {
+    return `Olá ${algumNome}, tudo bem?`
+  }
+
+  return <>{nome && <p>{gerarSaudacao(nome)}</p>}</>
+}
+
+export default Saudacao

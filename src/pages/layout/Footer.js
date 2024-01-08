a17b553512e0423d7ff35b216b78036d931fd1c6
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-01-08 12:01:44 -0300

    React Icons

--- src/pages/layout/Footer.js
+++ src/pages/layout/Footer.js
@@ -1,9 +1,16 @@
-import { Link } from 'react-router-dom'
-
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return(
-    <footer>Rodapé</footer>
+    <footer>
+      <ul className={styles.social_list}>
+        <li className={styles.item}><FaFacebook /></li>
+        <li className={styles.item}><FaInstagram /></li>
+        <li className={styles.item}><FaLinkedin /></li>
+      </ul>
+      <p>Nosso rodapé</p>
+    </footer>
   )
 }
 
